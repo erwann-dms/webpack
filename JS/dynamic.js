53d0b50eb4e103bfc7a0ff51bae9106d750024f0
erwann-dms <erwann1.dumas@outlook.fr>
2024-11-21 11:57:32 +0100

    regarder le carrouselle dans team

--- JS/dynamic.js
+++ JS/dynamic.js
@@ -12,3 +12,32 @@ function lightmodeToggle() {
        var element = document.body;
         element.classList.toggle("light-toggle");
 } 
+
+const track = document.getElementById('carouselTrack');
+const prevBtn = document.getElementById('prevBtn');
+const nextBtn = document.getElementById('nextBtn');
+
+let currentIndex = 0;
+
+function updateButtons() {
+    prevBtn.disabled = currentIndex === 0;
+    nextBtn.disabled = currentIndex === track.children.length - 1;
+}
+
+prevBtn.addEventListener('click', () => {
+    if (currentIndex > 0) {
+        currentIndex--;
+        track.style.transform = `translateX(-${currentIndex * 100}%)`;
+        updateButtons();
+    }
+});
+
+nextBtn.addEventListener('click', () => {
+    if (currentIndex < track.children.length - 1) {
+        currentIndex++;
+        track.style.transform = `translateX(-${currentIndex * 100}%)`;
+        updateButtons();
+    }
+});
+
+updateButtons();
