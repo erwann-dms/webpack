6057c05b7df36e718f64b1723f07888ec04e8dd3
erwann-dms <erwann1.dumas@outlook.fr>
2024-11-28 09:36:33 +0100

    fdgf

--- src/index.php
+++ src/index.php
@@ -27,7 +27,7 @@
             <span class="slider"></span>
           </label>
       </div>
-      <a class="nav-link" href="/login"><button class="button-nav" role="button" style="text-decoration: none; color: inherit;">Login</button></a>
+      <a class="nav-link" href="/login/index.php"><button class="button-nav" role="button" style="text-decoration: none; color: inherit;">Login</button></a>
     </div>
   </nav>
 
