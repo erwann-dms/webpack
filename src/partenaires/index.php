55b6f0b1d4fa0d030e7f0389697bbcf8025783a4
erwann-dms <erwann-dms>
2024-11-28 20:36:46 +0100

    je comprends pas pourquoi ca fonctionne pas dans la page pratenaire mais le style puis on pourrais la faire autrement qu'un carouselle

--- src/partenaires/index.php
+++ src/partenaires/index.php
@@ -4,8 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/Style/style.css">
+    <script type="text/javascript" src="JS/dynamic.js" defer></script>
     <title>Nos Partenaires Développeurs</title>
+    <link rel="shortcut icon" href="/photos/logo1.png">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
+
 <body class="body">
 <?php include $_SERVER['DOCUMENT_ROOT'] . "./common/navbar.php";?>
     <div>
