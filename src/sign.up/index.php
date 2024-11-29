41a8edf1ac386d475c53d2c9bdd8feaa7ce14958
Ambrozic Quentin <qambrozic@guardiaschool.fr>
2024-11-29 10:02:45 +0100

    login et sign up

--- src/sign.up/index.php
+++ src/sign.up/index.php
@@ -10,35 +10,34 @@
 </head>
 <body class="body-background">
 
-    <!-- Neige en fond -->
-    <div class="snowflakes" aria-hidden="true">
-        <!-- Flocons générés dynamiquement par JavaScript -->
-    </div>
-
     <div class="login-container">
-        <h2 class="title">Se Connecter</h2>
+        <h2 class="title">S'inscrire</h2>
 
-        
+     
         <form action="#" method="post" class="form">
-            
+          
             <input type="text" name="username" placeholder="Nom d'utilisateur" class="input" required><br>
             
-            
+          
             <input type="password" name="password" placeholder="Mot de passe" class="input" required><br>
             
-            <input type="submit" value="Connexion" class="submit-button"><br>
+        
+            <input type="submit" value="Inscription" class="submit-button"><br>
 
             <?php if (isset($_GET['error']) && $_GET['error'] === 'username_taken') : ?>
-                <p class="error">Nom d'utilisateur ou mot de passe erroné.</p>
+                <p class="error">Nom d'utilisateur déjà utilisé.</p>
             <?php endif; ?> 
+
             
             <div class="small-text">
-                <p>Pas encore inscrit ? <a href="../sign.up" class="link">Créer un compte</a></p>
+                <p>Déjà inscrit ? <a href="../login" class="link">J'ai déjà un compte</a></p>
                 <p><a href="#" class="link">Mot de passe oublié ?</a></p>
             </div>
         </form>
 
+     
         <button class="back-button" onclick="window.history.back()">Retour</button>
     </div>
+
 </body>
 </html>
