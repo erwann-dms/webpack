ce72361e58d685f6a36b20f8e259c4c10850ca5a
mrwn111 <mrwnchb111@gmail.com:mrwn111>
2024-11-29 09:53:39 +0100

    faute d'orthographe de quentinho

--- src/team/index.php
+++ src/team/index.php
@@ -37,11 +37,13 @@
               <div class="photo-container"><img src="../photos/Ambqpic.jpg" alt=""></div>
                 <div class="name">Quentin AMBROZIC</div>
                   <div class="bio">
-                    <p>Je m’appelle Quentin, j’ai 18 ans et je suis originaire de Saint Cyr au Mont d’Or, dans la périphérie de l’agglomération lyonnaise.
-                    J’ai commencé à m'intéresser à l’informatique il y à 3 ans quand j’ai choisi les composants et construit mon PC fixe seul pour noël 2021. J’ai pu acquérir des connaissances en hardware. Suite à ça je me suis intéressé à la partie software en 2022 j’ai commencé à apprendre des bases de python de manière autodidacte . 
-                    J’ai pris connaissance de la cybersécurité cette même année lors d’une porte ouverte à Eurexpo pour avoir une idée de carrière vers laquelle je voudrais me diriger . Depuis je me suis inscrit et été admis à Guardia Cybersecurity School. Grâce à cette école j’ai pus accumulé des connaissances avec les différents intervenants.
-                    Ceci est un petit apperçu de ce que j’ai pus apprendre au cours du Bachelor que j’ai passé dans cette école: 
-                    J’ai pus obtenir des connaissances approfondies sur du DevSecOps ,pentest ,SIR ,OSINT...
+                    <p>
+Je m’appelle Quentin, j’ai 18 ans et je suis originaire de Saint-Cyr au Mont-d’Or, dans la périphérie de l’agglomération lyonnaise.
+J’ai commencé à m’intéresser à l’informatique il y a 3 ans quand j’ai choisi les composants et construit mon PC fixe seul pour Noël 2021. J’ai pu acquérir des connaissances en Hardware. Suite à ça, je me suis intéressé à la partie software. En 2022, j’ai commencé à apprendre des bases de Python de manière autodidacte.
+J’ai pris connaissance de la cybersécurité cette même année lors d’une porte ouverte à Eurexpo pour avoir une idée de carrière vers laquelle je voudrais me diriger. Depuis, je me suis inscrit et été admis à Guardia Cybersecurity School. Grâce à cette école, j’ai pu accumuler des connaissances avec les différents intervenants.
+Ceci est un petit aperçu de ce que j’ai pu apprendre au cours du Bachelor que j’ai passé dans cette école :
+J’ai pu obtenir des connaissances approfondies sur du DevSecOps, Pentest, SIR, OSINT…
+
                     </p>
                   </div>
                   <div class="buttons">
