ca6922f73a3a4fd3ca9d83b7dd575c5eb1aa5d9b
pburgniard <pburgniard@guardiaschool.fr>
2024-12-02 10:22:29 +0100

    fromage

--- src/login/index.php
+++ src/login/index.php
@@ -1,9 +1,31 @@
 <?php
-    
+$conn = new mysqli("mysql", "root",$_SERVER['MYSQL_ROOT_PASSWORD'],"Website");
+if ($conn->connect_error) {
+    die("connection failed :" . $conn->connect_error);
+}
+
+$name = $password = "";
+$nameErr = $passwordErr = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Collect and sanitize form data
+    if (empty($_POST["name"])) {
+        $nameErr = "Nom requis";
+    } else {
+        $name = htmlspecialchars($_POST["name"]);
+    }
+
+    if (empty($_POST["password"])) {
+        $passwordErr = "Mot de passe requis";
+    } else {
+        $password = htmlspecialchars($_POST["password"]);
+    }
+}
 ?>
 
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -12,6 +34,7 @@
     <link rel="shortcut icon" href="/photos/logo1.png" type="image/x-icon">
     <script src="/JS/dynamic.js"></script>
 </head>
+
 <body class="body-background">
 
     <!-- Neige en fond -->
@@ -22,20 +45,21 @@
     <div class="login-container">
         <h2 class="title">Se Connecter</h2>
 
-        
+
         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post" class="form">
-            
-            <input type="text" name="username" placeholder="Nom d'utilisateur" class="input" required><br>
-            
-            
-            <input type="password" name="password" placeholder="Mot de passe" class="input" required><br>
-            
+
+            <input type="text" name="username" placeholder="Nom d'utilisateur" class="input"><br>
+
+
+            <input type="password" name="password" placeholder="Mot de passe" class="input">
+            <span style="color:red"><?php echo $passwordErr; ?></span><br>
+
             <input type="submit" value="Connexion" class="submit-button"><br>
 
-            <?php if (isset($_GET['error']) && $_GET['error'] === 'username_taken') : ?>
+            <?php if (isset($_GET['error']) && $_GET['error'] === 'username_taken'): ?>
                 <p class="error">Nom d'utilisateur ou mot de passe erroné.</p>
-            <?php endif; ?> 
-            
+            <?php endif; ?>
+
             <div class="small-text">
                 <p>Pas encore inscrit ? <a href="/signup" class="link">Créer un compte</a></p>
                 <p><a href="#" class="link">Mot de passe oublié ?</a></p>
